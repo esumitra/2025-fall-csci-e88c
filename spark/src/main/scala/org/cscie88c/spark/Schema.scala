28aad2b65726f9f53d1d5efec217b44656667f63
au_user <alb8990csci>
2025-11-02 22:47:59 -0500

    uncommented need schema

--- spark/src/main/scala/org/cscie88c/spark/Schema.scala
+++ spark/src/main/scala/org/cscie88c/spark/Schema.scala
@@ -1,42 +1,42 @@
 package org.cscie88c.spark
 import org.apache.spark.sql.types._
 
-// case class ComboYellowTripTaxiZoneSchema(
-//     // These are all headers in the trip parquet file
-//     // VendorID: String,
-//     tpep_pickup_datetime: String, // Required
-//     tpep_dropoff_datetime: String, // Required
-//     // passenger_count: Int,
-//     trip_distance: Double, // Required
-//     // RatecodeID: String,
-//     // store_and_fwd_flag: Boolean,
-//     PULocationID: Int, // Required
-//     // Pickup info from TaxiZoneSchema table
-//     LocationIDPU: Int,
-//     BoroughPU: String,
-//     ZonePU: String,
-//     Service_zonePU: String,
-//     //
-//     // These are all headers in the trip parquet file
-//     DOLocationID: Int, // Required
-//     // Dropoff info from TaxiZoneSchema table
-//     LocationIDDO: Int,
-//     BoroughDO: String,
-//     ZoneDO: String,
-//     Service_zoneDO: String,
-//     // These are all headers in the trip parquet file
-//     // payment_type: Int,
-//     fare_amount: Double // Required
-//     // extra: Double,
-//     // mta_tax: Double,
-//     // tip_amount: Double,
-//     // tolls_amount: Double,
-//     // improvement_surcharge: Double,
-//     // total_amount: Double,
-//     // congestion_surcharge: Double,
-//     // Airport_fee: Double,
-//     // cbd_congestion_fee: Double
-// )
+case class ComboYellowTripTaxiZoneSchema(
+    // These are all headers in the trip parquet file
+    // VendorID: String,
+    tpep_pickup_datetime: String, // Required
+    tpep_dropoff_datetime: String, // Required
+    // passenger_count: Int,
+    trip_distance: Double, // Required
+    // RatecodeID: String,
+    // store_and_fwd_flag: Boolean,
+    PULocationID: Int, // Required
+    // Pickup info from TaxiZoneSchema table
+    LocationIDPU: Int,
+    BoroughPU: String,
+    ZonePU: String,
+    Service_zonePU: String,
+    //
+    // These are all headers in the trip parquet file
+    DOLocationID: Int, // Required
+    // Dropoff info from TaxiZoneSchema table
+    LocationIDDO: Int,
+    BoroughDO: String,
+    ZoneDO: String,
+    Service_zoneDO: String,
+    // These are all headers in the trip parquet file
+    // payment_type: Int,
+    fare_amount: Double // Required
+    // extra: Double,
+    // mta_tax: Double,
+    // tip_amount: Double,
+    // tolls_amount: Double,
+    // improvement_surcharge: Double,
+    // total_amount: Double,
+    // congestion_surcharge: Double,
+    // Airport_fee: Double,
+    // cbd_congestion_fee: Double
+)
 
 case class YellowTripSchema(
 
