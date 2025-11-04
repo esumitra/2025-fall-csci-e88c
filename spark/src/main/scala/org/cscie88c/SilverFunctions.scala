6ee6a308b60da100a159e03af4eb41502af23ba9
Thraxiis <gregorymarkbrooks@gmail.com>
2025-11-04 10:30:36 -0500

    fixed some of the listed errors and got output for review

--- spark/src/main/scala/org/cscie88c/SilverFunctions.scala
+++ spark/src/main/scala/org/cscie88c/SilverFunctions.scala
@@ -1,8 +1,17 @@
 package org.cscie88c
 
+import org.apache.spark.sql.{DataFrame, SparkSession, Dataset}
+import org.apache.spark.sql.functions._
+import org.apache.spark.sql.types._
+import org.apache.spark.sql.functions.{from_unixtime, hour, to_date, datediff}
+
 object SilverFunctions {
 
 // ability to handle DFs here was not working at all,
 // using SparkJob.scala -- sorry
+
+def getWeekData(df: DataFrame, weekNum: Int): DataFrame =
+  df.filter(col("Pickup_Week") === weekNum)
+
 
 }
